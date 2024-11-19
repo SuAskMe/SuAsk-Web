91f87c795feb0a974c2a6a8d103df12f9df93718
DreamingLri <3507415455@qq.com>
2024-11-19 10:37:46 +0800

    ✨ add background-img components and some default images

---
+++ src/components/background-img.vue
@@ -0,0 +1,32 @@
+<template>
+    <img :src="imgUrl" alt="background_img" />
+</template>
+
+<script setup lang="ts">
+import { defineProps, watch } from 'vue';
+
+const props = defineProps<{
+    img_index?: number;
+    img_url?: string;
+}>();
+
+
+let imgUrl = '';
+
+
+watch(
+    () => [props.img_index, props.img_url],
+    ([newImgIndex, newImgUrl]) => {
+        if (newImgUrl) {
+            imgUrl = String(newImgUrl);
+        } else if (newImgIndex !== undefined) {
+            imgUrl = new URL(`../assets/bg_imgs/${newImgIndex}.png`, import.meta.url).href;
+        } else {
+            imgUrl = '';
+        }
+    },
+    { immediate: true }
+);
+</script>
+
+<style scoped></style>
