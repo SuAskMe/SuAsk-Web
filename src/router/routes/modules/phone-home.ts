13c6f83075f83d3c5e1bf9057b6837457180bd21
DreamingLri <3507415455@qq.com>
2024-11-21 10:02:02 +0800

    ⚡️ make phone-home and web-home.ts router lazy load

--- src/router/routes/modules/phone-home.ts
+++ src/router/routes/modules/phone-home.ts
@@ -1,17 +1,15 @@
 import type { AppRouteRecordRaw } from "@/router/types";
-import PhoneHome from '@/views/phone/phone-home.vue'
-import Setting from '@/views/Setting.vue'
 
 
 export const PhoneHomeRoute: AppRouteRecordRaw = {
     path: '/phone',
     name: 'phone',
-    component: PhoneHome,
+    component: () => import('@/views/phone/phone-home.vue'),
     children: [
         {
             path: 'setting',
             name: 'phone-setting',
-            component: Setting,
+            component: () => import('@/views/Setting.vue'),
         }
     ]
 }
