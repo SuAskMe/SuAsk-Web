479c9ec74ab36a209e06b16115e15ab421a48ca6
DreamingLri <3507415455@qq.com>
2024-11-22 15:07:44 +0800

    🎨 add scss

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,11 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/styles/global.scss";`,
+            },
+        }
+    }
 })
