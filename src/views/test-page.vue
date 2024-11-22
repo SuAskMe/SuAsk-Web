af4830579981d63f9ff31e30cb219f4cfaa0b5e4
DreamingLri <3507415455@qq.com>
2024-11-22 10:57:35 +0800

    📝 rename Test Page

---
+++ src/views/test-page.vue
@@ -0,0 +1,9 @@
+<template>
+    <ask-dialog :visible="true" :fullscreen="false" teacher-name="苏钰鑫" />
+</template>
+
+<script setup lang='ts'>
+import AskDialog from '@/components/AskDialog.vue';
+</script>
+
+<style></style>
