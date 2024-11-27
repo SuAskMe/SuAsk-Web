579aa2c75ad0d4a07063cf7a8db8b2928bf0abbe
DreamingLri <3507415455@qq.com>
2024-11-27 16:50:32 +0800

    ➖ remove arco-design

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-import '@arco-design/web-vue/dist/arco.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import ArcoVue from '@arco-design/web-vue'
 import '../mock/index.js'
 import 'virtual:svg-icons-register';
 import SvgIcon from "@/components/svg-icon"
