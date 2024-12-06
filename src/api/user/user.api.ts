0ccaa976c38f8db429157349845005e4144cf4d8
DreamingLri <3507415455@qq.com>
2024-12-06 19:29:21 +0800

    ✨ 将所有Api函数名称都加上Api后缀

--- src/api/user/user.api.ts
+++ src/api/user/user.api.ts
@@ -4,13 +4,18 @@ import request from "@/utils/http/request";
 enum Api {
     UPDATE_PASSWORD = '/user/update-password',
     UPDATE_USER_INFO = '/user/update-user-info',
+    GET_USER_INFO = '/user/info',
 }
 
-export function updatePassword(password: string) {
+export async function updatePasswordApi(password: string) {
     return request.post(Api.UPDATE_PASSWORD, password);
 }
 
-export function updateUserInfo(userInfo: UpdateUser) {
+export async function updateUserInfoApi(userInfo: UpdateUser) {
     return request.post(Api.UPDATE_USER_INFO, userInfo);
+}
+
+export async function getUserInfoApi() {
+    return request.get(Api.GET_USER_INFO);
 }
 
