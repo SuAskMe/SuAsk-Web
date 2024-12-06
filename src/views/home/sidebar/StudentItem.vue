9de9f5b73ef1aca59cf82eb142bee11ccd465188
DreamingLri <3507415455@qq.com>
2024-12-06 19:31:26 +0800

    🐛 修复侧边栏中的“我的收藏”跳转路径

--- src/views/home/sidebar/StudentItem.vue
+++ src/views/home/sidebar/StudentItem.vue
@@ -7,7 +7,7 @@
         <control-panel-item id="stu-ask-history" icon="chat-history" text="历史提问"
             :clicked="selectedItem == 'stu-ask-history'" @updateSelected="updateSelected" jump-to-path="ask-history" />
         <control-panel-item id="stu-star" icon="star" text="我的收藏" :clicked="selectedItem == 'stu-star'"
-            @updateSelected="updateSelected" jump-to-path="my-star" />
+            @updateSelected="updateSelected" jump-to-path="my-favorite" />
         <control-panel-item id="stu-settings" icon="settings" text="设置" :clicked="selectedItem == 'stu-settings'"
             @updateSelected="updateSelected" jump-to-path="setting" />
         <control-panel-item id="stu-help" icon="question" text="帮助" :clicked="selectedItem == 'stu-help'"
