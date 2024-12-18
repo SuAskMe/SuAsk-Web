d1cb797fba17d7a5e81d18391bda3413a8f44441
AndyC5H10O5 <1915965336@qq.com>
2024-12-18 13:51:35 +0800

    用el-pagination组件实现了分页查询逻辑

--- src/views/home/content/my-favorites/MyFavorites.ts
+++ src/views/home/content/my-favorites/MyFavorites.ts
@@ -1,6 +1,9 @@
 import {
     Get,
     type GetFavoriteResponse,
+    GetPage,
+    type GetPageFavoriteResponse,
+    type GetPageFavoriteRequest,
 } from "./request";
 
 export interface FavoriteQuestion {
@@ -8,13 +11,18 @@ export interface FavoriteQuestion {
     title: string;
     contents: string;
     views: number;
-    favorite_at: string; 
+    favorite_at: string;
     // image_urls: string[];
 }
 
-export async function GetFavoriteList(
-    // page: number
-): Promise<FavoriteQuestion[]> {
+export interface PageOutput {
+    list: FavoriteQuestion[];
+    total: number;
+    size: number;
+}
+
+export async function GetFavoriteList(): Promise<FavoriteQuestion[]> {
+// page: number
     var res: GetFavoriteResponse;
     res = await Get();
     if (res.favorite_question_list) {
@@ -22,5 +30,29 @@ export async function GetFavoriteList(
     } else {
         console.log("空的！");
         return [];
+    }
+}
+
+export async function GetPageFavoriteList(
+    page: number
+): Promise<PageOutput> {
+    var req: GetPageFavoriteRequest = { page_idx: page };
+    var res: GetPageFavoriteResponse;
+    res = await GetPage(req);
+    if (res.page_favorite_question_list) {
+        var output: PageOutput = {
+            list: res.page_favorite_question_list,
+            total: res.total,
+            size: res.size,
+        };
+        return output;
+    } else {
+        console.log("空的！");
+        var output: PageOutput = {
+            list: [],
+            total: 0,
+            size: 0,
+        };
+        return output;
     }
 }
