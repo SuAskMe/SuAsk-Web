1c641eb8e559bb676e8bb625be802c7c2c71f35d
DreamingLri <3507415455@qq.com>
2024-12-26 14:23:03 +0800

    🐛 修改favorite单词错误

--- src/components/bubble-card/bubble-question/__BubbleQuestion__.vue
+++ src/components/bubble-card/bubble-question/__BubbleQuestion__.vue
@@ -15,9 +15,9 @@ interface BubbleQuestionProps {
     answerNum?: number;
     imageUrls?: string[];
     avatars?: string[];
-    isFavourite?: boolean;
+    isFavorite?: boolean;
     clickCard?: (key: any) => void;
-    clickFavourite?: (key: any) => void;
+    clickFavorite?: (key: any) => void;
 }
 const props = defineProps<BubbleQuestionProps>();
 const timeStr = computed(() => getTimeStr(props.timeStamp));
@@ -38,41 +38,20 @@ const imageContainer = computed(() =>
             <div class="q-title">{{ title }}</div>
             <div class="q-body">
                 <div v-if="!isMarkdown" class="text">{{ text }}</div>
-                <div
-                    v-else
-                    :class="'md-container' + (showAllMarkdown ? '-all' : '')"
-                >
-                    <MdPreview
-                        id="preview-only"
-                        :model-value="text"
-                        class="md-preview"
-                    />
+                <div v-else :class="'md-container' + (showAllMarkdown ? '-all' : '')">
+                    <MdPreview id="preview-only" :model-value="text" class="md-preview" />
                 </div>
                 <div v-if="imageContainer.hasImages" class="photos-container">
-                    <div
-                        class="preview-group"
-                        :style="{
-                            width: imageContainer.containerWidth,
-                            gap: imageContainer.gap + ' ' + imageContainer.gap,
-                        }"
-                    >
-                        <el-image
-                            @click.stop
-                            v-for="(img, index) in imageUrls"
-                            :key="img"
-                            :src="img"
-                            :style="{
-                                width: imageContainer.size,
-                                height: imageContainer.size,
-                                borderRadius: '6px',
-                            }"
-                            :preview-src-list="imageUrls"
-                            :initial-index="index"
-                            fit="cover"
-                            lazy
-                            infinite
-                            preview-teleported
-                        ></el-image>
+                    <div class="preview-group" :style="{
+                        width: imageContainer.containerWidth,
+                        gap: imageContainer.gap + ' ' + imageContainer.gap,
+                    }">
+                        <el-image @click.stop v-for="(img, index) in imageUrls" :key="img" :src="img" :style="{
+                            width: imageContainer.size,
+                            height: imageContainer.size,
+                            borderRadius: '6px',
+                        }" :preview-src-list="imageUrls" :initial-index="index" fit="cover" lazy infinite
+                            preview-teleported></el-image>
                     </div>
                 </div>
             </div>
@@ -85,56 +64,24 @@ const imageContainer = computed(() =>
             </div>
         </div>
         <div v-if="answerNum && answerNum > 0" class="card-footer">
-            <img
-                :src="avatar"
-                onerror="this.src='/src/assets/default-avatar.png'; this.onerror=null;"
-                v-for="(avatar, index) in avatars"
-                :key="index"
-                :style="{
+            <img :src="avatar" onerror="this.src='/src/assets/default-avatar.png'; this.onerror=null;"
+                v-for="(avatar, index) in avatars" :key="index" :style="{
                     marginLeft: index === 0 ? '16px' : '-12px',
                     zIndex: 1 - index,
-                }"
-                class="avatar"
-            />
-            <div
-                v-if="avatars && answerNum > avatars.length"
-                style="font-size: 32px; color: #66b0ff"
-            >
+                }" class="avatar" />
+            <div v-if="avatars && answerNum > avatars.length" style="font-size: 32px; color: #66b0ff">
                 ...
             </div>
             <div class="text">{{ answerNum }} 个回答</div>
-            <div
-                class="favourite"
-                :data-tips="isFavourite ? '取消收藏' : '收藏'"
-                @click.stop="clickFavourite(key)"
-            >
-                <svg-icon
-                    icon="bookmark"
-                    size="24"
-                    :color="isFavourite ? '#FFC107' : '#66b0ff'"
-                    :filled="isFavourite"
-                />
+            <div class="favourite" :data-tips="isFavorite ? '取消收藏' : '收藏'" @click.stop="clickFavorite(key)">
+                <svg-icon icon="bookmark" size="24" :color="isFavorite ? '#FFC107' : '#66b0ff'" :filled="isFavorite" />
             </div>
         </div>
         <div v-else class="card-footer">
-            <svg-icon
-                class="msg-icon"
-                icon="communicate_message"
-                size="24"
-                color="#66b0ff"
-            />
+            <svg-icon class="msg-icon" icon="communicate_message" size="24" color="#66b0ff" />
             <div class="text">发表一个回答...</div>
-            <div
-                class="favourite"
-                :data-tips="isFavourite ? '取消收藏' : '收藏'"
-                @click.stop="clickFavourite(key)"
-            >
-                <svg-icon
-                    icon="bookmark"
-                    size="24"
-                    :color="isFavourite ? '#FFC107' : '#66b0ff'"
-                    :filled="isFavourite"
-                />
+            <div class="favourite" :data-tips="isFavorite ? '取消收藏' : '收藏'" @click.stop="clickFavorite(key)">
+                <svg-icon icon="bookmark" size="24" :color="isFavorite ? '#FFC107' : '#66b0ff'" :filled="isFavorite" />
             </div>
         </div>
     </div>
