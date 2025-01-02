ded1a735908176417143405a599ede874f4c02bf
DreamingLri <3507415455@qq.com>
2025-01-02 17:07:57 +0800

    🐛 将favourite改为favorite

--- src/components/bubble-card/bubble-question/__BubbleQuestion__.vue
+++ src/components/bubble-card/bubble-question/__BubbleQuestion__.vue
@@ -16,12 +16,12 @@ interface BubbleQuestionProps {
     imageUrls?: string[];
     avatars?: string[];
     isFavorite?: boolean;
-    showFavourite?: boolean;
+    showFavorite?: boolean;
     clickCard?: (key: any) => void;
     clickFavorite?: (key: any) => void;
 }
 const props = withDefaults(defineProps<BubbleQuestionProps>(), {
-    showFavourite: true,
+    showFavorite: true,
 });
 const timeStr = computed(() => getTimeStr(props.timeStamp));
 const key = computed(() => {
@@ -47,41 +47,20 @@ const showDefaultAvatar = (index: number) => {
             <div class="q-title">{{ title }}</div>
             <div class="q-body">
                 <div v-if="!isMarkdown" class="text">{{ text }}</div>
-                <div
-                    v-else
-                    :class="'md-container' + (showAllMarkdown ? '-all' : '')"
-                >
-                    <MdPreview
-                        id="preview-only"
-                        :model-value="text"
-                        class="md-preview"
-                    />
+                <div v-else :class="'md-container' + (showAllMarkdown ? '-all' : '')">
+                    <MdPreview id="preview-only" :model-value="text" class="md-preview" />
                 </div>
                 <div v-if="imageContainer.hasImages" class="photos-container">
-                    <div
-                        class="preview-group"
-                        :style="{
-                            width: imageContainer.containerWidth,
-                            gap: imageContainer.gap + ' ' + imageContainer.gap,
-                        }"
-                    >
-                        <el-image
-                            @click.stop
-                            v-for="(img, index) in imageUrls"
-                            :key="img"
-                            :src="img"
-                            :style="{
-                                width: imageContainer.size,
-                                height: imageContainer.size,
-                                borderRadius: '6px',
-                            }"
-                            :preview-src-list="imageUrls"
-                            :initial-index="index"
-                            fit="cover"
-                            lazy
-                            infinite
-                            preview-teleported
-                        ></el-image>
+                    <div class="preview-group" :style="{
+                        width: imageContainer.containerWidth,
+                        gap: imageContainer.gap + ' ' + imageContainer.gap,
+                    }">
+                        <el-image @click.stop v-for="(img, index) in imageUrls" :key="img" :src="img" :style="{
+                            width: imageContainer.size,
+                            height: imageContainer.size,
+                            borderRadius: '6px',
+                        }" :preview-src-list="imageUrls" :initial-index="index" fit="cover" lazy infinite
+                            preview-teleported></el-image>
                     </div>
                 </div>
             </div>
@@ -94,59 +73,25 @@ const showDefaultAvatar = (index: number) => {
             </div>
         </div>
         <div v-if="answerNum && answerNum > 0" class="card-footer">
-            <el-avatar
-                v-for="(avatar, index) in avatars"
-                :key="index"
-                :src="avatar"
-                :size="32"
-                :style="{
-                    marginLeft: index === 0 ? '16px' : '-16px',
-                    zIndex: 10 - index,
-                }"
-                class="avatar"
-                @error="showDefaultAvatar(index)"
-            />
-            <div
-                v-if="avatars && answerNum > avatars.length"
-                style="font-size: 32px; color: #66b0ff"
-            >
+            <el-avatar v-for="(avatar, index) in avatars" :key="index" :src="avatar" :size="32" :style="{
+                marginLeft: index === 0 ? '16px' : '-16px',
+                zIndex: 10 - index,
+            }" class="avatar" @error="showDefaultAvatar(index)" />
+            <div v-if="avatars && answerNum > avatars.length" style="font-size: 32px; color: #66b0ff">
                 ...
             </div>
             <div class="text">{{ answerNum }} 个回答</div>
-            <div
-                v-if="showFavourite"
-                class="favourite"
-                :data-tips="isFavorite ? '取消收藏' : '收藏'"
-                @click.stop="clickFavorite(key)"
-            >
-                <svg-icon
-                    icon="bookmark"
-                    size="24"
-                    :color="isFavorite ? '#FFC107' : '#66b0ff'"
-                    :filled="isFavorite"
-                />
+            <div v-if="showFavorite" class="favorite" :data-tips="isFavorite ? '取消收藏' : '收藏'"
+                @click.stop="clickFavorite(key)">
+                <svg-icon icon="bookmark" size="24" :color="isFavorite ? '#FFC107' : '#66b0ff'" :filled="isFavorite" />
             </div>
         </div>
         <div v-else class="card-footer">
-            <svg-icon
-                class="msg-icon"
-                icon="communicate_message"
-                size="24"
-                color="#66b0ff"
-            />
+            <svg-icon class="msg-icon" icon="communicate_message" size="24" color="#66b0ff" />
             <div class="text">发表一个回答...</div>
-            <div
-                v-if="showFavourite"
-                class="favourite"
-                :data-tips="isFavorite ? '取消收藏' : '收藏'"
-                @click.stop="clickFavorite(key)"
-            >
-                <svg-icon
-                    icon="bookmark"
-                    size="24"
-                    :color="isFavorite ? '#FFC107' : '#66b0ff'"
-                    :filled="isFavorite"
-                />
+            <div v-if="showFavorite" class="favorite" :data-tips="isFavorite ? '取消收藏' : '收藏'"
+                @click.stop="clickFavorite(key)">
+                <svg-icon icon="bookmark" size="24" :color="isFavorite ? '#FFC107' : '#66b0ff'" :filled="isFavorite" />
             </div>
         </div>
     </div>
