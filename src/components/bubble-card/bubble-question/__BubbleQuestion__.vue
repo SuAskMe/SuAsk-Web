5b801234039076a07af6b41ddf151a8d1f03c484
Jacko <1730072668@qq.com>
2025-01-05 12:52:56 +0800

    优化bubble-card组件的显示与交互

--- src/components/bubble-card/bubble-question/__BubbleQuestion__.vue
+++ src/components/bubble-card/bubble-question/__BubbleQuestion__.vue
@@ -42,56 +42,115 @@ const showDefaultAvatar = (index: number) => {
 </script>
 
 <template>
-    <div class="card-container" @click.stop="clickCard(key)">
-        <div class="card-body" :style="containerStyle">
-            <div class="q-title">{{ title }}</div>
-            <div class="q-body">
-                <div v-if="!isMarkdown" class="text">{{ text }}</div>
-                <div v-else :class="'md-container' + (showAllMarkdown ? '-all' : '')">
-                    <MdPreview id="preview-only" :model-value="text" class="md-preview" />
-                </div>
-                <div v-if="imageContainer.hasImages" class="photos-container">
-                    <div class="preview-group" :style="{
-                        width: imageContainer.containerWidth,
-                        gap: imageContainer.gap + ' ' + imageContainer.gap,
-                    }">
-                        <el-image @click.stop v-for="(img, index) in imageUrls" :key="img" :src="img" :style="{
-                            width: imageContainer.size,
-                            height: imageContainer.size,
-                            borderRadius: '6px',
-                        }" :preview-src-list="imageUrls" :initial-index="index" fit="cover" lazy infinite
-                            preview-teleported></el-image>
+    <div class="bubble-question">
+        <div class="card-container" @click.stop="clickCard(key)">
+            <div class="card-body" :style="containerStyle">
+                <div class="q-title">{{ title }}</div>
+                <div class="q-body">
+                    <div v-if="!isMarkdown" class="text">{{ text }}</div>
+                    <div
+                        v-else
+                        :class="
+                            'md-container' + (showAllMarkdown ? '-all' : '')
+                        "
+                    >
+                        <MdPreview
+                            id="preview-only"
+                            :model-value="text"
+                            class="md-preview"
+                        />
+                    </div>
+                    <div
+                        v-if="imageContainer.hasImages"
+                        class="photos-container"
+                    >
+                        <div
+                            class="preview-group"
+                            :style="{
+                                width: imageContainer.containerWidth,
+                                gap:
+                                    imageContainer.gap +
+                                    ' ' +
+                                    imageContainer.gap,
+                            }"
+                        >
+                            <el-image
+                                @click.stop
+                                v-for="(img, index) in imageUrls"
+                                :key="img"
+                                :src="img"
+                                :style="{
+                                    width: imageContainer.size,
+                                    height: imageContainer.size,
+                                    borderRadius: '6px',
+                                }"
+                                :preview-src-list="imageUrls"
+                                :initial-index="index"
+                                fit="cover"
+                                lazy
+                                infinite
+                                preview-teleported
+                            ></el-image>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="q-footer">
-                <div class="looks">
-                    <svg-icon icon="eye" size="16" color="#818181" />
-                    <span class="counts">{{ views }}</span>
+                <div class="q-footer">
+                    <div class="looks">
+                        <svg-icon icon="eye" size="16" color="#818181" />
+                        <span class="counts">{{ views }}</span>
+                    </div>
+                    <div class="time">{{ timeStr }}</div>
                 </div>
-                <div class="time">{{ timeStr }}</div>
             </div>
-        </div>
-        <div v-if="answerNum && answerNum > 0" class="card-footer">
-            <el-avatar v-for="(avatar, index) in avatars" :key="index" :src="avatar" :size="32" :style="{
-                marginLeft: index === 0 ? '16px' : '-16px',
-                zIndex: 10 - index,
-            }" class="avatar" @error="showDefaultAvatar(index)" />
-            <div v-if="avatars && answerNum > avatars.length" style="font-size: 32px; color: #66b0ff">
-                ...
+            <div v-if="answerNum && answerNum > 0" class="card-footer">
+                <el-avatar
+                    v-for="(avatar, index) in avatars"
+                    :key="index"
+                    :src="avatar"
+                    :size="32"
+                    :style="{
+                        marginLeft: index === 0 ? '16px' : '-14px',
+                        zIndex: 10 - index,
+                    }"
+                    class="avatar"
+                    @error="showDefaultAvatar(index)"
+                />
+                <div class="text">{{ answerNum }} 个回答</div>
+                <div
+                    v-if="showFavorite"
+                    class="favorite"
+                    :data-tips="isFavorite ? '取消收藏' : '收藏'"
+                    @click.stop="clickFavorite(key)"
+                >
+                    <svg-icon
+                        icon="bookmark"
+                        size="24"
+                        :color="isFavorite ? '#FFC107' : '#66b0ff'"
+                        :filled="isFavorite"
+                    />
+                </div>
             </div>
-            <div class="text">{{ answerNum }} 个回答</div>
-            <div v-if="showFavorite" class="favorite" :data-tips="isFavorite ? '取消收藏' : '收藏'"
-                @click.stop="clickFavorite(key)">
-                <svg-icon icon="bookmark" size="24" :color="isFavorite ? '#FFC107' : '#66b0ff'" :filled="isFavorite" />
-            </div>
-        </div>
-        <div v-else class="card-footer">
-            <svg-icon class="msg-icon" icon="communicate_message" size="24" color="#66b0ff" />
-            <div class="text">发表一个回答...</div>
-            <div v-if="showFavorite" class="favorite" :data-tips="isFavorite ? '取消收藏' : '收藏'"
-                @click.stop="clickFavorite(key)">
-                <svg-icon icon="bookmark" size="24" :color="isFavorite ? '#FFC107' : '#66b0ff'" :filled="isFavorite" />
+            <div v-else class="card-footer">
+                <svg-icon
+                    class="msg-icon"
+                    icon="communicate_message"
+                    size="24"
+                    color="#66b0ff"
+                />
+                <div class="text">发表一个回答...</div>
+                <div
+                    v-if="showFavorite"
+                    class="favorite"
+                    :data-tips="isFavorite ? '取消收藏' : '收藏'"
+                    @click.stop="clickFavorite(key)"
+                >
+                    <svg-icon
+                        icon="bookmark"
+                        size="24"
+                        :color="isFavorite ? '#FFC107' : '#66b0ff'"
+                        :filled="isFavorite"
+                    />
+                </div>
             </div>
         </div>
     </div>
