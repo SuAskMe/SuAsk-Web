87dd3583449567b4f0de4cd89f51dea08a115049
DreamingLri <3507415455@qq.com>
2025-01-05 12:23:14 +0800

    ✨ 添加home界面

---
+++ src/router/routes/modules/home.ts
@@ -0,0 +1,35 @@
+import { LeftSidebar } from "@/components/left-sidebar";
+import type { AppRouteRecordRaw } from "@/router/types";
+import { isMobile } from "@/utils/device";
+
+const PhoneHome = () => import("@/layout/home/PhoneHome.vue");
+const WebHome = () => import("@/layout/home/WebHome.vue");
+
+const Home = () => import("@/views/home");
+
+export const HomeRoute: AppRouteRecordRaw = {
+
+    path: "/home",
+    name: "HomeRoot",
+    meta: {
+        title: "主页",
+    },
+    props: {
+        userType: "student",
+        deviceType: isMobile() ? "phone" : "desktop",
+    },
+    component: isMobile() ? PhoneHome : WebHome,
+    children: [
+        {
+            path: "",
+            name: "Home",
+            meta: {
+                title: "主页",
+            },
+            components: {
+                default: Home,
+                // left_side: LeftSidebar,
+            },
+        },
+    ]
+}
---
+++ src/views/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main">
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { UserStore } from '@/store/modules/user';
+import { computed } from 'vue';
+
+const store = UserStore()
+
+
+</script>
+
+<style scoped lang="scss">
+.main {
+    display: flex;
+}
+</style>
