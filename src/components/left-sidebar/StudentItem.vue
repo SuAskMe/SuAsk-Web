139085d387767431c1b8c96864c03999c56f1e1c
DreamingLri <3507415455@qq.com>
2025-01-06 17:18:35 +0800

    ⚡️ 重构控制面板组件，改用pinia进行持久化管理（人话：刷新界面之后点击状态不会掉）

--- src/components/left-sidebar/StudentItem.vue
+++ src/components/left-sidebar/StudentItem.vue
@@ -1,22 +1,20 @@
 <template>
     <div class="control-panel-e">
         <control-panel-item class="control-panel-item" id="stu-ask-all" icon="group" text="问大家"
-            :clicked="selectedItem == 'stu-ask-all'" @updateSelected="updateSelected" jump-to-path="/ask-all" />
+            jump-to-path="/ask-all" />
         <control-panel-item class="control-panel-item" id="stu-ask-teacher" icon="graduation-cap" text="问老师"
-            :clicked="selectedItem == 'stu-ask-teacher'" @updateSelected="updateSelected" jump-to-path="/ask-teacher" />
+            jump-to-path="/ask-teacher" />
         <control-panel-item class="control-panel-item" id="stu-ask-history" icon="chat-history" text="历史提问"
-            :clicked="selectedItem == 'stu-ask-history'" @updateSelected="updateSelected" jump-to-path="/ask-history" />
+            jump-to-path="/ask-history" />
         <control-panel-item class="control-panel-item" id="stu-star" icon="star" text="我的收藏"
-            :clicked="selectedItem == 'stu-star'" @updateSelected="updateSelected" jump-to-path="/my-favorite" />
+            jump-to-path="/my-favorite" />
         <control-panel-item class="control-panel-item" id="stu-settings" icon="settings" text="设置"
-            :clicked="selectedItem == 'stu-settings'" @updateSelected="updateSelected" jump-to-path="/setting" />
-        <control-panel-item class="control-panel-item" id="stu-help" icon="question" text="帮助"
-            :clicked="selectedItem == 'stu-help'" @updateSelected="updateSelected" jump-to-path="/help" />
+            jump-to-path="/setting" />
+        <control-panel-item class="control-panel-item" id="stu-help" icon="question" text="帮助" jump-to-path="/help" />
     </div>
 </template>
 
 <script setup lang='ts'>
-import { updateSelected, selectedItem } from './selectedItem';
 import ControlPanelItem from '@/components/control-panel-item';
 
 </script>
