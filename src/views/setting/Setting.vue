7c0ef1135cbd5631e9d8d59ac1300ce114309407
DreamingLri <3507415455@qq.com>
2025-03-01 18:23:11 +0800

    ✨ 重构设置界面，替换滚动条组件，优化样式并调整响应式布局

--- src/views/setting/Setting.vue
+++ src/views/setting/Setting.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-scrollbar height="100vh">
+    <!-- <el-scrollbar height="100vh">
         <div class="main">
             <div class="title">
                 <p>基础信息</p>
@@ -27,27 +27,104 @@
                             <template #icon>
                                 <svg-icon icon="edit" color="#808080" />
                             </template>
+编辑
+</el-button>
+</div>
+<p> @{{ basicInfo.name }} </p>
+</div>
+</div>
+<div class="theme-and-button">
+    <p>主题</p>
+    <div class="theme">
+        <theme-image v-model="basicInfo.themeId" :src="imgList" :width="deviceType == 'desktop' ? '100px' : '70px'" />
+    </div>
+    <div class="button" v-if="deviceType == 'desktop'">
+        <el-button @click="updateUserInfo" type="primary">保存更改</el-button>
+    </div>
+</div>
+<div v-if="userStore.getRole() == 'teacher'" class="title">
+    <hr />
+</div>
+<div v-if="userStore.getRole() == 'teacher'" class="change-perm">
+    <p>提问箱可见性</p>
+    <div>
+        <el-radio-group v-model="questionVisible" size="large">
+            <el-radio-button label="公开" value="public" />
+            <el-radio-button label="需登录" value="protected" />
+            <el-radio-button label="关闭" value="private" />
+        </el-radio-group>
+    </div>
+</div>
+<div class="title">
+    <hr />
+</div>
+<div class="danger-place">
+    <p class="danger-option" @click="resetPassword">重置密码</p>
+    <p class="danger-option" @click="showLogoutDialog">退出登录</p>
+</div>
+<reset-password-dialog v-model:visible="showResetPassword" />
+<logout-dialog v-model:visible="showLogout" />
+</div>
+</el-scrollbar> -->
+    <el-container class="container">
+        <el-header>
+            <QuestionHeader sidebar_btn @sidebar="sidebar" />
+        </el-header>
+        <el-main class="main-container">
+            <div class="title">
+                <p>基础信息</p>
+                <hr />
+            </div>
+            <div class="name-bio-avatar">
+                <div class="name-bio">
+                    <p>昵称</p>
+                    <div>
+                        <el-input v-model="basicInfo.nickname" placeholder="Please input" />
+                    </div>
+                    <p>简介</p>
+                    <div>
+                        <bio-panel v-model="basicInfo.introduction" />
+                    </div>
+                </div>
+                <div class="avatar-id">
+                    <div class="avatar">
+                        <el-avatar :size="150" :src="basicInfo.avatar">
+                            <img src="@/assets/default-avatar.png" />
+                        </el-avatar>
+                        <input type="file" ref="imgPicker" accept="image/png,image/jpeg,image/jpg" style="display: none"
+                            multiple @change="pickImageImpl">
+                        <el-button @click.stop="pickImage" class="upload-btn" type="default" size="small">
+                            <template #icon>
+                                <svg-icon icon="edit" color="#808080" />
+                            </template>
                             编辑
                         </el-button>
                     </div>
-                    <p> @{{ basicInfo.name }} </p>
+                    <p class="id"> @{{ basicInfo.name }} </p>
                 </div>
             </div>
-            <div class="theme-and-button">
+            <div class="title">
                 <p>主题</p>
+                <hr />
+            </div>
+            <div class="theme">
                 <div class="theme">
                     <theme-image v-model="basicInfo.themeId" :src="imgList"
-                        :width="deviceType == 'desktop' ? '100px' : '70px'" />
-                </div>
-                <div class="button" v-if="deviceType == 'desktop'">
-                    <el-button @click="updateUserInfo" type="primary">保存更改</el-button>
+                        :width="deviceTypeStore.isMobile ? '75px' : '100px'" />
                 </div>
             </div>
+            <div class="title">
+                <hr />
+            </div>
+            <div class="button">
+                <el-button @click="updateUserInfo" type="primary">保存更改</el-button>
+            </div>
             <div v-if="userStore.getRole() == 'teacher'" class="title">
+                <p>提问箱可见性</p>
                 <hr />
             </div>
             <div v-if="userStore.getRole() == 'teacher'" class="change-perm">
-                <p>提问箱可见性</p>
+
                 <div>
                     <el-radio-group v-model="questionVisible" size="large">
                         <el-radio-button label="公开" value="public" />
@@ -59,33 +136,40 @@
             <div class="title">
                 <hr />
             </div>
-            <div class="danger-place">
+            <div class="password-logout">
                 <p class="danger-option" @click="resetPassword">重置密码</p>
                 <p class="danger-option" @click="showLogoutDialog">退出登录</p>
             </div>
-            <reset-password-dialog v-model:visible="showResetPassword" />
-            <logout-dialog v-model:visible="showLogout" />
-        </div>
-    </el-scrollbar>
+            <div style="height: 50px;"></div>
+        </el-main>
+    </el-container>
 </template>
 
 <script setup lang='ts'>
 import { computed, inject, onMounted, ref, watch } from 'vue';
-import BioPanel from '@/components/bio-panel';
-import ThemeImage from './ThemeImage.vue';
-import SvgIcon from '@/components/svg-icon';
 import { ElMessage } from 'element-plus';
-import ResetPasswordDialog from './ResetPasswordDialog.vue';
-import LogoutDialog from './LogoutDialog.vue';
 import { getUserInfoApi, updateUserInfoApi } from '@/api/user/user.api';
 import { UserStore } from '@/store/modules/user';
 import { updateTeacherPermApi } from '@/api/teacher/teacher.api';
+import QuestionHeader from '@/components/question-header';
+import { SidebarStore } from '@/store/modules/sidebar';
+import BioPanel from '@/components/bio-panel/BioPanel.vue';
+import { DeviceTypeStore } from '@/store/modules/device-type';
+import ThemeImage from './ThemeImage.vue';
 
 const imgList = ref<string[]>([]);
 const images = import.meta.glob('@/assets/bg_imgs/*.png', { eager: true });
 imgList.value = Object.values(images).map((module) => (module as any).default);
 
-const deviceType = inject('deviceType', 'desktop');
+// const deviceType = inject('deviceType', 'desktop');
+
+const deviceTypeStore = DeviceTypeStore();
+
+const sidebarStore = SidebarStore();
+
+function sidebar() {
+    sidebarStore.toggle();
+}
 
 const imgPicker = ref<HTMLInputElement>();
 
@@ -153,11 +237,11 @@ function showLogoutDialog() {
     showLogout.value = true;
 }
 
-const questionVisible = ref(userStore.getUser().question_box_perm) 
+const questionVisible = ref(userStore.getUser().question_box_perm)
 
 watch(() => questionVisible.value, async (newVal) => {
-    await updateTeacherPermApi(questionVisible.value).then(async res =>{
-        if(res) {
+    await updateTeacherPermApi(questionVisible.value).then(async res => {
+        if (res) {
             ElMessage.success('保存成功')
             let user = userStore.getUser()
             user.question_box_perm = questionVisible.value
@@ -170,7 +254,7 @@ watch(() => questionVisible.value, async (newVal) => {
 
 </script>
 
-<style lang="scss" scoped src="./desktop.scss">
+<style lang="scss" scoped src="./setting.scss">
 :deep(.el-dialog) {
     border-radius: 15px;
 }
