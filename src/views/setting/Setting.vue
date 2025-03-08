baa82f889cdc848fb7793f3b2145652416022880
Jacko <1730072668@qq.com>
2025-03-08 22:31:18 +0800

    style: 压缩背景图片

--- src/views/setting/Setting.vue
+++ src/views/setting/Setting.vue
@@ -13,7 +13,10 @@
                     <div class="name-bio">
                         <p>昵称</p>
                         <div>
-                            <el-input v-model="basicInfo.nickname" placeholder="Please input" />
+                            <el-input
+                                v-model="basicInfo.nickname"
+                                placeholder="Please input"
+                            />
                         </div>
                         <p>简介</p>
                         <div>
@@ -25,16 +28,27 @@
                             <el-avatar :size="150" :src="basicInfo.avatar">
                                 <img src="@/assets/default-avatar.png" />
                             </el-avatar>
-                            <input type="file" ref="imgPicker" accept="image/png,image/jpeg,image/jpg"
-                                style="display: none" multiple @change="pickImageImpl">
-                            <el-button @click.stop="pickImage" class="upload-btn" type="default" size="small">
+                            <input
+                                type="file"
+                                ref="imgPicker"
+                                accept="image/png,image/jpeg,image/jpg"
+                                style="display: none"
+                                multiple
+                                @change="pickImageImpl"
+                            />
+                            <el-button
+                                @click.stop="pickImage"
+                                class="upload-btn"
+                                type="default"
+                                size="small"
+                            >
                                 <template #icon>
                                     <svg-icon icon="edit" color="#808080" />
                                 </template>
                                 编辑
                             </el-button>
                         </div>
-                        <p class="id"> @{{ basicInfo.name }} </p>
+                        <p class="id">@{{ basicInfo.name }}</p>
                     </div>
                 </div>
                 <div class="title">
@@ -43,21 +57,29 @@
                 </div>
                 <div class="theme-picker">
                     <div class="theme">
-                        <theme-image v-model="basicInfo.themeId" :src="imgList"
-                            :width="deviceTypeStore.isMobile ? '75px' : '100px'" />
+                        <theme-image
+                            v-model="basicInfo.themeId"
+                            :src="imgList"
+                            :width="deviceTypeStore.isMobile ? '75px' : '100px'"
+                        />
                     </div>
                 </div>
                 <div class="title">
                     <hr />
                 </div>
                 <div class="button">
-                    <el-button @click="updateUserInfo" type="primary">保存更改</el-button>
+                    <el-button @click="updateUserInfo" type="primary"
+                        >保存更改</el-button
+                    >
                 </div>
                 <div v-if="userStore.getRole() == 'teacher'" class="title">
                     <p>提问箱可见性</p>
                     <hr />
                 </div>
-                <div v-if="userStore.getRole() == 'teacher'" class="change-perm">
+                <div
+                    v-if="userStore.getRole() == 'teacher'"
+                    class="change-perm"
+                >
                     <div>
                         <el-radio-group v-model="questionVisible" size="large">
                             <el-radio-button label="公开" value="public" />
@@ -71,9 +93,11 @@
                 </div>
                 <div class="password-logout">
                     <p class="danger-option" @click="resetPassword">重置密码</p>
-                    <p class="danger-option" @click="showLogoutDialog">退出登录</p>
+                    <p class="danger-option" @click="showLogoutDialog">
+                        退出登录
+                    </p>
                 </div>
-                <div style="height: 50px;"></div>
+                <div style="height: 50px"></div>
             </el-main>
         </el-scrollbar>
         <reset-password-dialog v-model:visible="showResetPassword" />
@@ -81,22 +105,22 @@
     </el-container>
 </template>
 
-<script setup lang='ts'>
-import { computed, inject, onMounted, ref, watch } from 'vue';
-import { ElMessage } from 'element-plus';
-import { getUserInfoApi, updateUserInfoApi } from '@/api/user/user.api';
-import { UserStore } from '@/store/modules/user';
-import { updateTeacherPermApi } from '@/api/teacher/teacher.api';
-import QuestionHeader from '@/components/question-header';
-import { SidebarStore } from '@/store/modules/sidebar';
-import BioPanel from '@/components/bio-panel/BioPanel.vue';
-import { DeviceTypeStore } from '@/store/modules/device-type';
-import ThemeImage from './ThemeImage.vue';
-import ResetPasswordDialog from './ResetPasswordDialog.vue';
-import LogoutDialog from './LogoutDialog.vue';
+<script setup lang="ts">
+import { computed, inject, onMounted, ref, watch } from "vue";
+import { ElMessage } from "element-plus";
+import { getUserInfoApi, updateUserInfoApi } from "@/api/user/user.api";
+import { UserStore } from "@/store/modules/user";
+import { updateTeacherPermApi } from "@/api/teacher/teacher.api";
+import QuestionHeader from "@/components/question-header";
+import { SidebarStore } from "@/store/modules/sidebar";
+import BioPanel from "@/components/bio-panel/BioPanel.vue";
+import { DeviceTypeStore } from "@/store/modules/device-type";
+import ThemeImage from "./ThemeImage.vue";
+import ResetPasswordDialog from "./ResetPasswordDialog.vue";
+import LogoutDialog from "./LogoutDialog.vue";
 
 const imgList = ref<string[]>([]);
-const images = import.meta.glob('@/assets/bg_imgs/*.png', { eager: true });
+const images = import.meta.glob("@/assets/bg_imgs/*.jpg", { eager: true });
 imgList.value = Object.values(images).map((module) => (module as any).default);
 
 // const deviceType = inject('deviceType', 'desktop');
@@ -113,55 +137,56 @@ const imgPicker = ref<HTMLInputElement>();
 
 function pickImage() {
     if (imgPicker.value) {
-        imgPicker.value.click()
+        imgPicker.value.click();
     }
 }
 
 function pickImageImpl(event: any) {
-    const file = event.target.files
+    const file = event.target.files;
     if (file.length > 1) {
-        ElMessage.error('最多上传一张头像')
-        return
+        ElMessage.error("最多上传一张头像");
+        return;
     }
-    avatarFile.value = file[0]
-    basicInfo.value.avatar = URL.createObjectURL(file[0])
+    avatarFile.value = file[0];
+    basicInfo.value.avatar = URL.createObjectURL(file[0]);
 }
 
-const avatarFile = ref<File | null>(null)
+const avatarFile = ref<File | null>(null);
 
 const userStore = UserStore();
 
-const basicInfo = computed(() => userStore.getUser())
+const basicInfo = computed(() => userStore.getUser());
 
 async function updateUserInfo() {
-    let formData = new FormData()
-    formData.append('nickname', basicInfo.value.nickname)
-    formData.append('introduction', basicInfo.value.introduction)
-    formData.append('themeId', (basicInfo.value.themeId).toString())
+    let formData = new FormData();
+    formData.append("nickname", basicInfo.value.nickname);
+    formData.append("introduction", basicInfo.value.introduction);
+    formData.append("themeId", basicInfo.value.themeId.toString());
 
-    formData.append('avatar', avatarFile.value!)
+    formData.append("avatar", avatarFile.value!);
 
-    await updateUserInfoApi(formData).then(async res => {
-        if (res) {
-            ElMessage.success('保存成功')
-            await getUserInfoApi().then(res => {
-                if (res) {
-                    userStore.setUser(res)
-                } else {
-                    ElMessage.error('保存失败1')
-                }
-            })
-        } else {
-            ElMessage.error('保存失败2')
-        }
-    }).catch(err => {
-        console.log(err)
-        ElMessage.error('保存失败3')
-        return
-    })
+    await updateUserInfoApi(formData)
+        .then(async (res) => {
+            if (res) {
+                ElMessage.success("保存成功");
+                await getUserInfoApi().then((res) => {
+                    if (res) {
+                        userStore.setUser(res);
+                    } else {
+                        ElMessage.error("保存失败1");
+                    }
+                });
+            } else {
+                ElMessage.error("保存失败2");
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            ElMessage.error("保存失败3");
+            return;
+        });
 }
-onMounted(() => {
-})
+onMounted(() => {});
 
 const showResetPassword = ref(false);
 
@@ -175,21 +200,23 @@ function showLogoutDialog() {
     showLogout.value = true;
 }
 
-const questionVisible = ref(userStore.getUser().question_box_perm)
+const questionVisible = ref(userStore.getUser().question_box_perm);
 
-watch(() => questionVisible.value, async (newVal) => {
-    await updateTeacherPermApi(questionVisible.value).then(async res => {
-        if (res) {
-            ElMessage.success('保存成功')
-            let user = userStore.getUser()
-            user.question_box_perm = questionVisible.value
-            userStore.setUser(user)
-        } else {
-            ElMessage.error('保存失败')
-        }
-    })
-})
-
+watch(
+    () => questionVisible.value,
+    async (newVal) => {
+        await updateTeacherPermApi(questionVisible.value).then(async (res) => {
+            if (res) {
+                ElMessage.success("保存成功");
+                let user = userStore.getUser();
+                user.question_box_perm = questionVisible.value;
+                userStore.setUser(user);
+            } else {
+                ElMessage.error("保存失败");
+            }
+        });
+    }
+);
 </script>
 
 <style lang="scss" scoped src="./setting.scss">
