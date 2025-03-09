bc0fadf0dde1fedd2e39c24f5f28d4355e2c5c67
Jacko <1730072668@qq.com>
2025-03-09 14:25:08 +0800

    feat(api): 增加心跳接口及登录后自动跳转逻辑

--- src/api/user/login.api.ts
+++ src/api/user/login.api.ts
@@ -2,8 +2,9 @@ import type { LoginReq, LoginRes } from "@/model/user.model";
 import request from "@/utils/http/request";
 
 enum Api {
-    LOGIN = '/login',
-    LOGOUT = '/user/logout',
+    LOGIN = "/login",
+    LOGOUT = "/user/logout",
+    HEARTBEAT = "/user/heartbeat",
 }
 
 export async function loginApi(req: LoginReq): Promise<LoginRes> {
@@ -16,6 +17,14 @@ export async function loginApi(req: LoginReq): Promise<LoginRes> {
 
 export async function logoutApi() {
     return request.post(Api.LOGOUT).then((res) => {
+        if (res) {
+            return res.data;
+        }
+    });
+}
+
+export async function heartbeatApi() {
+    return request.post(Api.HEARTBEAT).then((res) => {
         if (res) {
             return res.data;
         }
