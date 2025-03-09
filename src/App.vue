43119cea4e1a357554f791d2f31d8c5d2a795855
Jacko <1730072668@qq.com>
2025-03-09 14:32:31 +0800

    feat(App): 添加窗口高度自适应功能 (Beta)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 初始化时设置高度
+function setRealVH() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+// 监听窗口变化（如地址栏隐藏、横竖屏切换）
+window.addEventListener("resize", setRealVH);
+window.addEventListener("orientationchange", setRealVH);
+
+// 首次加载执行
+setRealVH();
+</script>
 
 <template>
     <RouterView />
