160fc8b37ea746a364350746b2d9df4adfdb50d8
Jacko <1730072668@qq.com>
2025-03-09 15:39:52 +0800

    style: 优化移动端视图高度计算

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
+import { DeviceTypeStore } from "./store/modules/device-type";
 // 初始化时设置高度
+const deviceType = DeviceTypeStore();
 function setRealVH() {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
-
-// 监听窗口变化（如地址栏隐藏、横竖屏切换）
-window.addEventListener("resize", setRealVH);
-window.addEventListener("orientationchange", setRealVH);
-
-// 首次加载执行
-setRealVH();
+if (deviceType.isMobile) {
+    // 首次加载执行
+    setRealVH();
+    // 监听窗口变化（如地址栏隐藏）
+    window.addEventListener("resize", setRealVH);
+}
 </script>
 
 <template>
