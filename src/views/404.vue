cec6983766ab712f96ef3e46d58bdae8b0020b41
DreamingLri <3507415455@qq.com>
2025-03-14 15:15:43 +0800

    🎨 优化404界面

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,10 +1,30 @@
-<script setup lang="ts"></script>
-
 <template>
-    <div style="text-align: center">
-        <h1>该页面不存在呢QAQ</h1>
-        <h3>404 NotFound</h3>
+    <div class="main-container" @click="navigateToHome">
+        <div class="404-text">
+            <h1>该页面不存在呢QAQ</h1>
+            <h3>404 NotFound</h3>
+        </div>
     </div>
 </template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
 
-<style scoped></style>
+const router = useRouter()
+
+function navigateToHome() {
+    router.replace({ name: 'Root' })
+}
+</script>
+
+<style scoped>
+.main-container {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    height: 100lvh;
+
+    @media screen and (max-width: 768px) {
+        align-items: center;
+    }
+}
+</style>
