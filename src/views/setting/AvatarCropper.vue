aaae1fa2638349688df631f295f6da068e439ad1
DreamingLri <3507415455@qq.com>
2026-06-07 20:16:16 +0800

    refactor: wrap legacy settings views

--- src/views/setting/AvatarCropper.vue
+++ src/views/setting/AvatarCropper.vue
@@ -1,33 +1,5 @@
-<template>
-    <VueCropper
-        ref="cropper"
-        :img="img"
-        :auto-crop="true"
-        :fixed="true"
-        :fixed-number="[1, 1]"
-        :center-box="true"
-        output-type="png"
-    />
-</template>
+<script lang="ts">
+import AvatarCropper from '@/pages/settings/AvatarCropper.vue'
 
-<script setup lang="ts">
-import { ref } from 'vue'
-import { VueCropper } from 'vue-cropper'
-import 'vue-cropper/dist/index.css'
-
-defineProps<{
-    img: string
-}>()
-
-const cropper = ref<{
-    getCropBlob: (callback: (blob: Blob) => void) => void
-} | null>(null)
-
-function getCropBlob(callback: (blob: Blob) => void) {
-    cropper.value?.getCropBlob(callback)
-}
-
-defineExpose({
-    getCropBlob,
-})
+export default AvatarCropper
 </script>
